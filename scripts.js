e5aef4d07fa0a3550895e314bd8da425c2b88ae5
Sai Vanga <Sai@Sai.Rocks>
2021-08-09 15:30:15 -0700

    Added buttons and JS

--- scripts.js
+++ scripts.js
@@ -1,1 +1,50 @@
-console.log("Testing");
+const conFormEl = document.getElementById("contact-form");
+const nameEl = document.getElementById("name");
+const emailEl = document.getElementById("email");
+const phoneEl = document.getElementById("phone");
+
+const hideError = () => {
+  const errorEl = document.getElementById("error");
+  errorEl.style.display = "none";
+};
+
+const showError = (error) => {
+  const errorEl = document.getElementById("error");
+  errorEl.style.display = "block";
+  errorEl.textContent = error;
+};
+
+const validateInput = (name, email, phone) => {
+  hideError();
+
+  if (name !== "" && (email !== "" || phone !== "")) {
+    return true;
+  } else {
+    showError("Name and (email or phone) are mandatory!");
+    return false;
+  }
+};
+
+
+
+conFormEl.addEventListener("submit", (e) => {
+  // Stop submitting the form
+  e.preventDefault();
+
+  // Get input values
+  const name = nameEl.value.trim();
+  const email = emailEl.value.trim();
+  const phone = phoneEl.value.trim();
+
+  // Validate input
+  const isInputValid = validateInput(name, email, phone);
+  if (!isInputValid) {
+    return;
+  }
+
+  // Input is valid
+  console.log("Do next!");
+});
+
+// Make sure to put the cursor in name element
+nameEl.focus();
